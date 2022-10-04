976465cc14615795ecc0bd69ffed648ee6d7a23d
SmallPeenwow <bowdencamryn@gmail.com>
2022-10-04 23:41:23 +0200

    Login Page Done

---
+++ client/src/components/App.js
@@ -0,0 +1,10 @@
+import Login from './Login';
+import React, { useState } from 'react';
+
+function App() {
+	const [id, setId] = useState();
+
+	return <Login onIdSubmit={setId} />;
+}
+
+export default App;
