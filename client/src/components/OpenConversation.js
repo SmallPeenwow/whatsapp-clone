3e5a7a2855e935cee2e13a3bc61d304c5ab1044f
SmallPeenwow <bowdencamryn@gmail.com>
2022-10-07 16:10:43 +0200

    All Done Nice

--- client/src/components/OpenConversation.js
+++ client/src/components/OpenConversation.js
@@ -28,7 +28,11 @@ export default function OpenConversation() {
 					{selectedConversation.map((message, index) => {
 						const lastMessage = selectedConversation.messages.length - 1 === index;
 						return (
-							<div ref={lastMessage ? setRef : null} key={index} className={`my-1 d-flex flex-column ${message.fromMe ? 'align-self-end' : ''}`}>
+							<div
+								ref={lastMessage ? setRef : null}
+								key={index}
+								className={`my-1 d-flex flex-column ${message.fromMe ? 'align-self-end align-items-end' : 'align-items-start'}`}
+							>
 								<div className={`rounded px-2 py-1 ${message.fromMe ? 'bg-primary text-white' : 'border'}`}>{message.text}</div>
 								<div className={`text-muted small ${message.fromMe ? 'text-right' : ''}`}>{message.fromMe ? 'You' : message.senderName}</div>
 							</div>
