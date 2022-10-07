168d11e33ef2596baef3dcaada15d2cda1a28453
SmallPeenwow <bowdencamryn@gmail.com>
2022-10-07 12:56:06 +0200

    Server Setup Done

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const io = required('socket.io')(5000);
+
+io.on('connection', (socket) => {
+	const id = socket.handshake.query.id;
+	socket.join(id);
+
+	socket.on('send-message', ({ recipients, text }) => {
+		recipients.forEach((recipient) => {
+			const newRecipients = recipients.filter((r) => r !== recipient);
+			newRecipients.push(id);
+			socket.broadcast.to(recipient);
+		});
+	});
+});
